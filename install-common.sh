9eaf295708cc8ea11dce8113e99f8b321a48d4dd
Mitja Bezensek <mitja.bezensek@outfit7.com>
2017-02-22 09:23:32 +0100

    Add gitconfig.

--- install-common.sh
+++ install-common.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Installs common software
+echo "Copying gitconfig"
+cp ~/profile.d/gitconfig ~/.gitconfig
+
+echo "Installing common software"
 sudo apt-get install -y net-tools htop netcat telnet tmux dnsutils whois tcpdump lftp httpie
 
-# Java
-sudo apt-get install openjdk-8-jdk
+#sudo apt-get install openjdk-8-jdk
 
-# .vimrc config
-cat <<EOF >> ~/.vimrc
+
+echo "Configuring vim"
+cut <<EOF >> ~/.vimrc
 syntax on
 colorscheme peachpuff
 EOF
-
