e435f378fdc9db28a5eb3e6913dbb37f39366d98
Mitja Bezensek <mitjab@fuster.lj.si>
2017-06-26 21:36:43 +0200

    Add support for OS X. Add prompt.

--- install-common.sh
+++ install-common.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+os=$(uname)
+
 echo "Copying gitconfig"
 cp profile.d/gitconfig ~/.gitconfig
 
-echo "Installing common software"
-sudo apt-get install -y net-tools htop netcat telnet tmux dnsutils whois tcpdump lftp httpie
+echo "Copying bash profile"
+cp ~/.bash_profile ~/.bash_profile.original > /dev/null
+cp profile.d/userprofile.sh ~/.bash_profile
+
+if [ "$os" == "Linux" ]; then 
+  echo "Linux detected"
+  echo "Installing common software"
+  sudo apt-get install -y net-tools htop netcat telnet tmux dnsutils whois tcpdump lftp httpie
+fi
 
 #sudo apt-get install openjdk-8-jdk
 
+if [ "$os" == "Darwin" ]; then
+	echo "macOS detected"
+	echo "Installing bash_profile_osx"
+fi
 
 echo "Configuring vim"
 cat <<EOF >> ~/.vimrc
--- profile.d/userprofile.sh
+++ profile.d/userprofile.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-# Aliases
+os=$(uname)
 
+# Functions
+git_branch_prompt() {
+  branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
+  test -n "$branch" && echo "($branch) "
+}
+
+export PS1="\w \033[0;32m\$(git_branch_prompt)\033[0m[\A]$ "
+
+# Aliases
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
-alias ls='ls --color=auto'
+#alias ls='ls --color=auto'
 alias l='ls -CF'
 alias la='ls -A'
 alias ll='ls -alhF'
 alias ..='cd ..'
 alias ports='netstat -tulanp'
 alias guid="uuidgen | tr '[:upper:]' '[:lower:]'"
+
+if [ "$os" == "Darwin" ]; then
+  export LC_ALL=en_US.UTF-8
+  export LANG=en_US.UTF-8
+fi
+
