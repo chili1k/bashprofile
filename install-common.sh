3f69b30d992ca871c70b0957028954a8f525a49e
Mitja Bezensek <mitja.bezensek@outfit7.com>
2018-11-21 15:42:50 +0100

    Do apt update on start.

--- install-common.sh
+++ install-common.sh
@@ -12,7 +12,8 @@ cp profile.d/userprofile.sh ~/.bash_profile
 if [ "$os" == "Linux" ]; then 
   echo "Linux detected"
   echo "Installing common software"
-  sudo apt-get install -y net-tools htop netcat telnet tmux dnsutils whois tcpdump lftp httpie jq python-pip
+  sudo apt-get update && \
+    sudo apt-get install -y net-tools htop netcat telnet tmux dnsutils whois tcpdump lftp httpie jq python-pip
 fi
 
 #sudo apt-get install openjdk-8-jdk
